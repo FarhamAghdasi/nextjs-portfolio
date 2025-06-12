ecdabec8a908457c263ce7834957e9a9dead13e7
Farham Aghdasi <farhamaghdasi08@gmail.com>
2025-06-12 13:10:52 +0330

    added captcha for comment in blog

---
+++ src/components/captcha.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, ChangeEvent, FC } from 'react';
+
+type CreateCaptchaProps = {
+  onCaptchaChange: (value: string) => void;
+};
+
+const CreateCaptcha: FC<CreateCaptchaProps> = ({ onCaptchaChange }) => {
+  const [captchaCode, setCaptchaCode] = useState<string>('');
+  const [captchaImage, setCaptchaImage] = useState<string>('');
+
+  const generateCaptchaCode = (): string => {
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let captcha = '';
+    for (let i = 0; i < 6; i++) {
+      captcha += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return captcha;
+  };
+
+  const generateCaptchaImage = (code: string): string => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) return '';
+
+    const width = 200;
+    const height = 60;
+    canvas.width = width;
+    canvas.height = height;
+
+    const backgroundColor = '#f0f0f0';
+    const textColor = '#333';
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height);
+
+    const lineColor = '#ccc';
+    for (let i = 0; i < 5; i++) {
+      ctx.beginPath();
+      ctx.moveTo(Math.random() * width, Math.random() * height);
+      ctx.lineTo(Math.random() * width, Math.random() * height);
+      ctx.strokeStyle = lineColor;
+      ctx.stroke();
+    }
+
+    ctx.font = '30px Arial';
+    ctx.fillStyle = textColor;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(code, width / 2, height / 2);
+
+    return canvas.toDataURL('image/png');
+  };
+
+  const getCaptcha = (): void => {
+    const newCaptchaCode = generateCaptchaCode();
+    const newCaptchaImage = generateCaptchaImage(newCaptchaCode);
+    setCaptchaCode(newCaptchaCode);
+    setCaptchaImage(newCaptchaImage);
+  };
+
+  useEffect(() => {
+    getCaptcha();
+  }, []);
+
+  const handleCaptchaInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    onCaptchaChange(e.target.value);
+  };
+
+  return (
+    <div className="col-12">
+      <div className="form-group d-flex align-items-center mt-4">
+        <img
+          src={captchaImage}
+          alt="Captcha"
+          style={{ cursor: "pointer", marginRight: "10px" }}
+          onClick={getCaptcha}
+        />
+        <input
+          type="text"
+          name="captcha"
+          placeholder="Enter CAPTCHA"
+          onChange={handleCaptchaInputChange}
+          required
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CreateCaptcha;
