1b3de020e7d01adfd460ac4f4e73fb5e5b3d694c
Farham Aghdasi <farhamaghdasi08@gmail.com>
2025-10-07 19:43:17 +0330

    feat: full stack , og image , about intro

--- src/components/pages/about-page.tsx
+++ src/components/pages/about-page.tsx
@@ -8,10 +8,10 @@ const AboutPage: React.FC = () => {
   return (
     <>
       <Inner title="About Me" first="Home" secend="About Farham Aghdasi" />
-      <ScrollAnimation animationType="fadeInUp" duration={.5} delay={0.2}>
+      <ScrollAnimation animationType="fadeInUp" duration={0.5} delay={0.1}>
         <About />
       </ScrollAnimation>
-      <ScrollAnimation animationType="zoomIn" duration={1} delay={0.4}>
+      <ScrollAnimation animationType="zoomIn" duration={0.8} delay={0.2}>
         <Skills />
       </ScrollAnimation>
     </>
